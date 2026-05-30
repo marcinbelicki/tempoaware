03454e5e52ca0017df44539a3bbcf49f5738292c
Marcin Belicki <marcin.belicki@abaro.net>
2026-05-30 11:57:37 +0200

    [5] new message

--- src/main/scala/pl/belicki/tempoaware/command/response/OpenResponse.scala
+++ src/main/scala/pl/belicki/tempoaware/command/response/OpenResponse.scala
@@ -1,5 +1,5 @@
 package pl.belicki.tempoaware.command.response
 
 case object OpenResponse extends Response {
-  override val message: String = "Opened the task in the default browser."
+  override val message: String = "Opened the requested asset in the browser."
 }
